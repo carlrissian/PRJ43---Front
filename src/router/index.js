b8503b2edc829ce68aa6f60872a6a002863b7955
Antonio J. Jiménez <antoniojjg@gmail.com>
2025-08-05 11:03:32 +0200

    update 05/08/25

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,12 @@
-import { createRouter, createWebHistory } from 'vue-router'
-// Import your other views here
+import { createRouter, createWebHistory } from 'vue-router';
+import LoginView from '../views/Login.vue';
 
 const routes = [
-  // Define your SPA routes here, excluding /login
-]
+  { path: '/', redirect: '/login' },
+  { path: '/login', name: 'Login', component: LoginView },
+];
 
 export default createRouter({
   history: createWebHistory(),
   routes,
-})
+});
